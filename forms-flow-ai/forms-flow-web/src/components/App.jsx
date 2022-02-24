96133d106de1ff93f3a195a8e1cd570c520b4c1b
Andre Pestana <andre.pestana@aot-technologies.com>
2022-02-23 19:27:59 -0800

    Started customizations

---
+++ forms-flow-ai/forms-flow-web/src/components/App.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Provider } from "react-redux";
+import { ConnectedRouter } from "connected-react-router";
+import "../assets/styles/layouts.scss";
+import "../assets/styles/user-styles.css";
+import BaseRouting from "./BaseRouting";
+import {Helmet} from "react-helmet";
+import {KEYCLOAK_URL} from "../constants/constants";
+import "../assets/fonts/BCSans-Bold.woff"
+import "../assets/fonts/BCSans-BoldItalic.woff";
+import "../assets/fonts/BCSans-Italic.woff";
+import "../assets/fonts/BCSans-Regular.woff";
+
+const App = React.memo((props) => {
+    const { store, history } = props;
+    return (
+      <div>
+        <Helmet>
+          {KEYCLOAK_URL?<link rel="preconnect" href={KEYCLOAK_URL} />:null}
+        </Helmet>
+        <Provider store={store}>
+          <ConnectedRouter history={history}>
+            <BaseRouting store={store}/>
+          </ConnectedRouter>
+        </Provider>
+      </div>
+    );
+});
+
+App.propTypes = {
+  history: PropTypes.any.isRequired,
+  store: PropTypes.any.isRequired,
+};
+
+export default App;
---
+++ forms-flow-ai/forms-flow-web/src/containers/NavBar.jsx
@@ -0,0 +1,136 @@
+import React from "react";
+import {Navbar, Dropdown, Container, Nav, NavDropdown} from "react-bootstrap";
+import {Link, useLocation} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import UserService from "../services/UserService";
+import {getUserRoleName, getUserRolePermission, getUserInsightsPermission} from "../helper/user";
+
+import "./styles.scss";
+import {CLIENT, STAFF_REVIEWER, APPLICATION_NAME, STAFF_DESIGNER} from "../constants/constants";
+import ServiceFlowFilterListDropDown from "../components/ServiceFlow/filter/ServiceTaskFilterListDropDown";
+import {push} from "connected-react-router";
+
+const NavBar = React.memo(() => {
+  const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
+  const location = useLocation();
+  const { pathname } = location;
+  const user = useSelector((state) => state.user.userDetail);
+  const userRoles = useSelector((state) => state.user.roles);
+  const showApplications= useSelector((state) => state.user.showApplications);
+  const dispatch = useDispatch();
+  const logoPath = "/logo.svg?1";
+  const appName = APPLICATION_NAME;
+
+  const logout = () => {
+      dispatch(push(`/`));
+      UserService.userLogout();
+  }
+
+  const goToTask = () => {
+    dispatch(push(`/task`));
+  }
+
+  return (
+    <header>
+      <Navbar expand="lg" className="topheading-border-bottom" fixed="top" style={{backgroundColor: '#003366'}}>
+        <Container fluid>
+          {/*<Nav className="d-lg-none">
+            <div className="mt-1" onClick={menuToggle}>
+              <i className="fa fa-bars fa-lg"/>
+            </div>
+          </Nav>*/}
+          <Navbar.Brand className="d-flex" >
+            <Link to="/">
+              <img
+                
+                src={logoPath}
+                width="150"
+                height="90"
+                alt="Logo"
+              />
+            </Link>
+            <div className="custom-app-name">{appName}</div>
+          </Navbar.Brand>
+         {/*
+           <Navbar.Brand className="d-flex">
+            <Link to="/">
+                  <img
+                    className="img-xs rounded-circle"
+                    src="/assets/Images/user.svg"
+                    alt="profile"
+                  />
+            </Link>
+          </Navbar.Brand>*/}
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          {isAuthenticated?
+            <Navbar.Collapse id="responsive-navbar-nav" className="navbar-nav">
+            <Nav id="main-menu-nav" className="mr-auto active">
+              <Nav.Link as={Link} to='/form'  className={`main-nav nav-item ${
+                pathname.match(/^\/form/) ? "active-tab" : ""
+              }`}> Forms</Nav.Link>
+              {(getUserRolePermission(userRoles, STAFF_DESIGNER)) ?
+                (<Nav.Link as={Link} to='/admin'  className={`main-nav nav-item ${
+                  pathname.match(/^\/admin/) ? "active-tab" : ""
+                }`}> Admin</Nav.Link>)
+                :null}
+
+              {showApplications?(getUserRolePermission(userRoles, STAFF_REVIEWER) ||  getUserRolePermission(userRoles, CLIENT)) ?
+                (<Nav.Link as={Link} to='/application'  className={`main-nav nav-item ${
+                  pathname.match(/^\/application/) ? "active-tab" : ""
+                }`}> Applications</Nav.Link>)
+                :null:
+                null}
+
+{/*              {getUserRolePermission(userRoles, STAFF_REVIEWER) ?
+                <Nav.Link as={Link} to='/task'  className={`main-nav nav-item ${
+                  pathname.match(/^\/task/) ? "active-tab" : ""
+                }`}><i className="fa fa-list"/> Tasks</Nav.Link>
+                :
+                null}*/}
+
+              {getUserRolePermission(userRoles, STAFF_REVIEWER) ?
+                <NavDropdown title={<> Tasks</>} id="task-dropdown"
+                             className={`main-nav nav-item taskDropdown ${pathname.match(/^\/task/) ? "active-tab-dropdown" : ""}`} onClick={goToTask}>
+                  <ServiceFlowFilterListDropDown/>
+              </NavDropdown>:null}
+
+              {getUserRolePermission(userRoles, STAFF_REVIEWER) ?<NavDropdown title={<>Dashboards</>}
+                                                                              id="dashboard-dropdown"
+                                                                              className={`main-nav nav-item ${
+                                                                                pathname.match(/^\/metrics/) || pathname.match(/^\/insights/) ? "active-tab-dropdown" : ""
+                                                                              }`}>
+                <NavDropdown.Item as={Link} to='/metrics' className={`main-nav nav-item ${
+                  pathname.match(/^\/metrics/) ? "active-tab" : ""
+                }`}><i className="fa fa-pie-chart fa-fw fa-lg"  /> Metrics</NavDropdown.Item>
+               {getUserInsightsPermission() && <NavDropdown.Item as={Link} to='/insights' className={`main-nav nav-item ${
+                  pathname.match(/^\/insights/) ? "active-tab" : ""
+                }`}><i className="fa fa-lightbulb-o fa-fw fa-lg"/> Insights</NavDropdown.Item>}
+              </NavDropdown>:null}
+            </Nav>
+            <Nav className="ml-auto">
+                  <Dropdown alignRight>
+                    <Dropdown.Toggle id="dropdown-basic" as="div">
+                   <span className="mr-1">
+                   <i className="fa fa-user fa-lg"/>
+                    </span>
+                      <span id="username-dropdown" className="d-none d-lg-inline-block">
+                      {user?.name || user?.preferred_username || ""}
+                  </span>
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                      <Dropdown.Item> {user?.name || user?.preferred_username}<br/>
+                        <i className="fa fa-users fa-lg fa-fw"/>
+                        <b>{getUserRoleName(userRoles)}</b></Dropdown.Item>
+                      <Dropdown.Divider/>
+                      <Dropdown.Item onClick ={logout}><i className="fa fa-sign-out fa-fw"/> Logout</Dropdown.Item>
+                    </Dropdown.Menu>
+                  </Dropdown>
+                </Nav>
+          </Navbar.Collapse>:null}
+        </Container>
+      </Navbar>
+    </header>
+  );
+})
+
+export default NavBar;
