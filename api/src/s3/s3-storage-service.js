2c178581d5006051fbf8c33581504514150d2a7e
vameh-aot <victor.ameh@aot-technologies.com>
2022-03-15 12:21:24 -0700

    added s3 env variables

--- api/src/s3/s3-storage-service.js
+++ api/src/s3/s3-storage-service.js
@@ -1,61 +1,26 @@
 var aws4  = require('aws4');
-var fs = require('fs');
+//var fs = require('fs');
 
 
-// aws4 will sign an options object as you'd pass to http.request, with an AWS service and region
-var opts = { host: 'ag-pssg-sharedservices.objectstore.gov.bc.ca', path: '/ag-pssg-jsb-srvdoc-dev-bkt', service: 'execute-api', region: 'us-west-1' }
- 
-// aws4.sign() will sign and modify these options, ready to pass to http.request
-aws4.sign(opts, { accessKeyId: 'AKIA2CD6BD9A0C8EC26C', secretAccessKey: 'b/i6oHBTOy5OYui+85vvSo9J4jVJuMlor6rargAG' })
+// Get Enviroment variables
+var BUCKETNAME = process.env.S3_BUCKETNAME;
+var ACCESS_KEY_ID = process.env.S3_ACCESS_KEY_ID;
+var SECRET_ACCESS_KEY = process.env.S3_SECRET_ACCESS_KEY;
+var HOST = process.env.S3_HOST;
+var REGION = process.env.S3_REGION;
+var SERVICE = process.env.S3_SERVICE;
 
-console.log(opts)
+function getAuthorizationKey() {
 
-
-
-
-/* 
-var ep = new AWS.Endpoint({
-   host: 'ag-pssg-sharedservices.objectstore.gov.bc.ca',
-   port: 443,
-   protocol: 'https'
-
-});
-
-
-
-const s3 = new AWS.S3({
-    //accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    //secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-    accessKeyId: 'AKIA2CD6BD9A0C8EC26C',
-    secretAccessKey: 'b/i6oHBTOy5OYui+85vvSo9J4jVJuMlor6rargAG',
-    AWS_S3_ENDPOINT:'https://ag-pssg-sharedservices.objectstore.gov.bc.ca',
-    endpoint: ep
-     
-  })
-
-   s3.listBuckets( (err, data) => {
-    if (err) {
-      console.log(err)
-    }
-    //resolve(data.Location)
-    console.log(err)
-  })
-
-
-const filename = 'portal-main-page.pdf'
-const fileContent = fs.readFileSync(filename)
-
-const params = {
-  // Bucket: process.env.AWS_BUCKET_NAME,
-  Bucket: 'ag-pssg-jsb-srvdoc-dev-bkt',
-  Key: `${filename}.pdf`,
-  Body: fileContent
+  // aws4 will sign an options object as you'd pass to http.request, with an AWS service and region
+  var opts = { host: HOST, path: BUCKETNAME, service: SERVICE, region: REGION }
+   
+  // aws4.sign() will sign and modify these options, ready to pass to http.request
+  aws4.sign(opts, { accessKeyId: ACCESS_KEY_ID, secretAccessKey: SECRET_ACCESS_KEY })
+  
+  console.log(opts)
+  return opts;
 }
 
-s3.upload(params, (err, data) => {
-  if (err) {
-    console.log(err)
-  }
-  //resolve(data.Location)
-  console.log(err)
-}) */
+
+
