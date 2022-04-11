0659ef574870d283c2df5cd24a2271b3ce3ec985
Andre Pestana <andre.pestana@aot-technologies.com>
2022-04-11 15:48:21 -0700

    Added connection to mssql

--- api/src/model/index.js
+++ api/src/model/index.js
@@ -9,24 +9,39 @@ require('dotenv').config();
 // 	benchmark: true
 // });
 
-//const sequelize = new Sequelize(process.env.DB_CONNECTION_URL);
 const DB_NAME = process.env.DB_NAME;
 const DB_USERNAME = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_HOST = process.env.DB_HOST;
 const DB_PORT = process.env.DB_PORT;
-console.log(process.env.DB_NAME)
-const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
-	host: DB_HOST,
-	port: DB_PORT,
-	dialect: 'postgres', //'mssql',
-	// dialectOptions: {
-	//   options: {
-	// 	// useUTC: false,
-	// 	// dateFirst: 1,
-	//   }
-	// }
-  })
+const DB_USE_POSTGRES = process.env.DB_USE_POSTGRES;
+
+let sequelize = null;
+
+if (DB_USE_POSTGRES==="true") {
+	console.log('Connecting to Postgres DB')
+	console.log('DB_NAME:', DB_NAME)
+	sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
+		host: DB_HOST,
+		port: DB_PORT,
+		dialect: 'postgres',
+	})
+} else {
+	console.log('Connecting to MSSQL DB')
+	console.log('DB_NAME:', DB_NAME)
+	sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
+		host: DB_HOST,
+		port: DB_PORT,
+		dialect: 'mssql',
+		dialectOptions: {
+		  options: {
+			// useUTC: false,
+			// dateFirst: 1,
+		  }
+		}
+	})
+}
+
 
 const modelDefiners = [
 	require('./attachment.model'),
